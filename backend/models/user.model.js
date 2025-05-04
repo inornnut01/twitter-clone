cacbb3fbda4674a14d96be77521f23ac0bf298e8
inornnut01 <inornnut01@gmail.com>
2025-05-04 12:41:43 +0700

    signup auth

---
+++ backend/models/user.model.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId, // 16 character
+      ref: "User",
+      default: [],
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: [],
+    },
+  ],
+
+  profileImg: {
+    type: String,
+    default: "",
+  },
+  coverImg: {
+    type: String,
+    default: "",
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  link:{
+    type: String,
+    default: "",
+  },
+
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+// use the schema to create a model
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connect.db.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json()); // parse the body of the request
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 
