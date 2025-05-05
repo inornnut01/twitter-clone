b92a2299b224af71bbdec41a5ed8cbe8cf11991c
inornnut01 <inornnut01@gmail.com>
2025-05-05 10:00:26 +0700

    notification routes and controllers added

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,35 @@
+import Notification from "../models/notification.model.js";
+
+
+export const getNotifications = async (req, res) => {
+  try {
+
+    const userId = req.user._id;
+
+    const notifications = await Notification.find({to: userId})
+    .populate({
+      path: "from",
+      select: "username profileImg"
+    });
+
+    await Notification.updateMany({to:userId}, {read:true});
+
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.log("Error in getNotifications controller", error.message);
+    res.status(500).json({message: "Internal server error"});
+  }
+};
+
+export const deleteNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    await Notification.deleteMany({to: userId});
+
+    res.status(200).json({message: "All notifications deleted"});
+  } catch (error) {
+    console.log("Error in deleteNotification controller", error.message);
+    res.status(500).json({message: "Internal server error"});
+  }
+};
