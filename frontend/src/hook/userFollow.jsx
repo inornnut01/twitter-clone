e45d3233efa2c6c19632b619fa437e153ddcadbf
inornnut01 <inornnut01@gmail.com>
2025-05-12 15:45:10 +0700

    like post mutatiomn added

--- frontend/src/hook/userFollow.jsx
+++ frontend/src/hook/userFollow.jsx
@@ -24,7 +24,7 @@ const useFollow = () => {
   },
   onSuccess: () => {
     Promise.all([
-      queryClient.invalidateQueries({queryKey: ["suggestedUsers"]}),
+      queryClient.invalidateQueries({queryKey: ["suggestedUsers"]}), 
       queryClient.invalidateQueries({queryKey: ["authUser"]}),
     ]);
   },
